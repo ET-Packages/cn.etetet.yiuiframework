91657204569b29abca647c9ebd8b87a36cbbd66e
Lsy <379338943@qq.com>
2024-10-19 22:49:44 +0800

    永久缓存不显示时间

--- Runtime/Core/YIUIBind/Code/CDE/UIBindCDETable_Editor.cs
+++ Runtime/Core/YIUIBind/Code/CDE/UIBindCDETable_Editor.cs
@@ -65,7 +65,7 @@ namespace YIUIFramework
         [MinValue(0.01)]
         public float CachePanelTime = 10;
 
-        private bool ShowCachePanelTime => PanelOption.HasFlag(EPanelOption.TimeCache);
+        private bool ShowCachePanelTime => PanelOption.HasFlag(EPanelOption.TimeCache) && !PanelOption.HasFlag(EPanelOption.ForeverCache);
 
         [LabelText("同层级时 优先级高的在前面")] //相同时后开的在前
         [ShowIf("UICodeType", EUICodeType.Panel)]
